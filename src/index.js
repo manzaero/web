d115dbf899059fc0e09710b4f126495ca5486807
manzaero <ligoero@gmail.com>
2024-09-11 23:11:53 +0300

    webpack task number 4

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+import './index.css';
+import IMAGE from './img.png'
+
 console.log("Hello world!");
 
-// const container = document.createElement('div');
-// container.className = 'container';
-// const h1 = document.createElement('h1');
-// h1.textContent = 'I love JavaScript';
-// const img = document.createElement('img');
-// img.className = 'img';
-// img.src = IMAGE;
-// container.append(h1, img)
-// document.body.append(container)
+const container = document.createElement('div');
+container.className = 'container';
+const h1 = document.createElement('h1');
+h1.textContent = 'I love JavaScript';
+const img = document.createElement('img');
+img.className = 'img';
+img.src = IMAGE;
+container.append(h1, img)
+document.body.append(container)
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'index.html')
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ]
     },
 };
