91de2a687ee0dc7c7868a9e2dc7c4fd2a5280008
manzaero <ligoero@gmail.com>
2024-09-11 23:25:04 +0300

    webpack task number 4

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'docs'),
+        path: path.resolve(__dirname, 'dist'),
     },
     plugins: [new HtmlWebpackPlugin({
         template: path.resolve(__dirname, 'index.html')
