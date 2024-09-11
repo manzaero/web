d01b45ca791df2f61f7bdf89698c27f65aa22c1b
manzaero <ligoero@gmail.com>
2024-09-11 22:49:09 +0300

    webpack task number 3

---
+++ src/index.js
@@ -0,0 +1,11 @@
+console.log("Hello world!");
+
+// const container = document.createElement('div');
+// container.className = 'container';
+// const h1 = document.createElement('h1');
+// h1.textContent = 'I love JavaScript';
+// const img = document.createElement('img');
+// img.className = 'img';
+// img.src = IMAGE;
+// container.append(h1, img)
+// document.body.append(container)
